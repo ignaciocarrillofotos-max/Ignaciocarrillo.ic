51f5270d258e12fad9b0216f88be8bc90586a592
ignaciocarrillofotos-max <ignaciocarrillo.fotos@gmail.com>
2026-06-11 21:51:38 +0200

    Add files via upload

---
+++ WEBstatica_NAKAS/galeria.js
@@ -0,0 +1,15 @@
+function openFolder(folder){
+
+    if(folder === "deportes"){
+        window.location.href = "FOTOSVIDEOS/deportes/";
+    }
+
+    if(folder === "fauna"){
+        window.location.href = "FOTOSVIDEOS/fauna/";
+    }
+
+    if(folder === "paisajes"){
+        window.location.href = "FOTOSVIDEOS/paisajes/";
+    }
+
+}
