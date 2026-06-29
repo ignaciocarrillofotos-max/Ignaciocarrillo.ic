696d984644b9e7444dbf7485c34ef605f65489a2
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-06-29 23:34:29 +0200

    Update galeria.js

--- galeria.js
+++ galeria.js
@@ -217,6 +217,8 @@ lightboxImg.addEventListener("wheel",(e)=>{
 escala = 1;
 lightboxImg.style.transform="scale(1)";
 
-
+lightbox.style.backgroundImage=`url(${imagenes[indiceActual].src})`;
+lightbox.style.backgroundSize="cover";
+lightbox.style.backgroundPosition="center";
 
 
