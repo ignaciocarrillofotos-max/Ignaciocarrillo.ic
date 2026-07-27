cc53ec1aea51817b4ff1acde22869f5eb753620a
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-28 00:25:30 +0200

    Update galeria.js

--- galeria.js
+++ galeria.js
@@ -32,13 +32,18 @@ if(tipoGaleria !== "fotos"){
     galeriaActual.forEach((src,index)=>{
         const photo = document.createElement("div");
         photo.classList.add("photo");
-
+        
+const watermark = document.createElement("img");
+watermark.src = "../FOTOSVIDEOS/logos/mi_Firma_blanco_semientera.png";
+watermark.className = "watermark";
+        
         const img = document.createElement("img");
         img.src = src;
         img.loading = "lazy";
         img.dataset.index = index;
 
         photo.appendChild(img);
+        photo.appendChild(watermark);
         gallery.appendChild(photo);
 
     });
