20482bb3cbd6adfadf9a70c55963608a16ba9bfd
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-27 19:09:17 +0200

    Create deportes.js with sports photo paths
    Added an array of photo paths for sports.

---
+++ GALERIA/deportes.js
@@ -0,0 +1,17 @@
+const fotos = [
+
+"../FOTOSVIDEOS/metaabrazo.JPG",
+"../FOTOSVIDEOS/deportes/YoDuatlon.jpg",
+"../FOTOSVIDEOS/deportes/DSC00699.JPG",
+"../FOTOSVIDEOS/deportes/DSC00823.JPG",
+"../FOTOSVIDEOS/deportes/DSC00848.JPG",
+"../FOTOSVIDEOS/deportes/DSC01011.JPG",
+"../FOTOSVIDEOS/deportes/DSC01279.JPG",
+"../FOTOSVIDEOS/deportes/DSC01281.JPG",
+"../FOTOSVIDEOS/deportes/DSC01283.JPG",
+"../FOTOSVIDEOS/deportes/DSC02636.JPG",
+"../FOTOSVIDEOS/deportes/DSC02680.JPG",
+"../FOTOSVIDEOS/deportes/abrazo.JPG",
+"../FOTOSVIDEOS/deportes/foto6.jpg"
+
+];
