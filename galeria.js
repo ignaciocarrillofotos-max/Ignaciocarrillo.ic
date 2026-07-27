f9e911238287d2f79176247350b39e855808c073
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-28 01:11:47 +0200

    Include watermark in gallery images
    Added watermark to images in the gallery.

--- galeria.js
+++ galeria.js
@@ -32,12 +32,20 @@ const photo = document.createElement("div");
 photo.classList.add("photo");
 
 const img = document.createElement("img");
+
 img.src = src;
 img.loading = "lazy";
 img.dataset.index = index;
 
+
+const watermark = document.createElement("img");
+watermark.src = "../FOTOSVIDEOS/logos/mi_Firma_blanco_letras.png";
+watermark.classList.add("gallery-watermark");
+
 photo.appendChild(img);
+photo.appendChild(watermark);
 gallery.appendChild(photo);
+        
     });
 
 }
