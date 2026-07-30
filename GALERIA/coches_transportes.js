0962f0f84f892212aee5581a4428e6e537eb56bb
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-30 17:42:10 +0200

    Update coches_transportes.js

--- GALERIA/coches_transportes.js
+++ GALERIA/coches_transportes.js
@@ -1,1 +1,10 @@
+const nombreGaleria = "Bodas";
+const tipoGaleria = "fotos";
+const galeriaActual = [
 
+    "../FOTOSVIDEOS/coches_transportes/coches_transportes-1.jpg",
+    "../FOTOSVIDEOS/coches_transportes/coches_transportes-2.jpg",
+    "../FOTOSVIDEOS/coches_transportes/coches_transportes-3.jpg",
+    "../FOTOSVIDEOS/coches_transportes/coches_transportes-4.jpg"
+
+];
