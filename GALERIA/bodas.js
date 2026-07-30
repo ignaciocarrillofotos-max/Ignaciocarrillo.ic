d2cfdd44bd283aa4c14b954eefca40014424afa2
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-30 17:25:53 +0200

    Update bodas.js

--- GALERIA/bodas.js
+++ GALERIA/bodas.js
@@ -1,1 +1,19 @@
+const nombreGaleria = "Bodas";
+const tipoGaleria = "fotos";
+const galeriaActual = [
 
+    "../FOTOSVIDEOS/bodas/boda-01.jpg",
+    "../FOTOSVIDEOS/bodas/boda-02.jpg",
+    "../FOTOSVIDEOS/bodas/boda-03.jpg",
+    "../FOTOSVIDEOS/bodas/boda-04.PNG",
+    "../FOTOSVIDEOS/bodas/boda-05.JPG",
+    "../FOTOSVIDEOS/bodas/boda-06.JPG",
+    "../FOTOSVIDEOS/bodas/boda-07.JPG",
+    "../FOTOSVIDEOS/bodas/boda-08.JPG",
+    "../FOTOSVIDEOS/bodas/boda-09.jpg",
+    "../FOTOSVIDEOS/bodas/boda-10.jpg",
+    "../FOTOSVIDEOS/bodas/boda-11.jpg",
+    "../FOTOSVIDEOS/bodas/boda-12.jpg",
+    "../FOTOSVIDEOS/bodas/boda-13.jpg",
+    "../FOTOSVIDEOS/bodas/boda-14.jpg"
+];
