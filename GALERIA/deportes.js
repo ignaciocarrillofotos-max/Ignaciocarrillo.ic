6d84e1532c4d81674bbf6d68a3c6149d10392004
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-30 21:12:05 +0200

    Update deportes.js

--- GALERIA/deportes.js
+++ GALERIA/deportes.js
@@ -3,18 +3,34 @@ const nombreGaleria = "Deportes";
 const tipoGaleria = "fotos";
 const galeriaActual = [
 
-    "../FOTOSVIDEOS/metaabrazo.JPG",
-    "../FOTOSVIDEOS/deportes/YoDuatlon.jpg",
-    "../FOTOSVIDEOS/deportes/DSC00699.JPG",
-    "../FOTOSVIDEOS/deportes/DSC00823.JPG",
-    "../FOTOSVIDEOS/deportes/DSC00848.JPG",
-    "../FOTOSVIDEOS/deportes/DSC01011.JPG",
-    "../FOTOSVIDEOS/deportes/DSC01279.JPG",
-    "../FOTOSVIDEOS/deportes/DSC01281.JPG",
-    "../FOTOSVIDEOS/deportes/DSC01283.JPG",
-    "../FOTOSVIDEOS/deportes/DSC02636.JPG",
-    "../FOTOSVIDEOS/deportes/DSC02680.JPG",
-    "../FOTOSVIDEOS/deportes/abrazo.JPG",
-    "../FOTOSVIDEOS/deportes/foto6.jpg"
+    "../FOTOSVIDEOS/deportes/deportes-01.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-02.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-03.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-04.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-05.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-06.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-07.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-08.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-09.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-10.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-11.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-12.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-13.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-14.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-15.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-16.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-17.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-18.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-19.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-20.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-21.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-22.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-23.jpg",
+    "../FOTOSVIDEOS/deportes/deportes-24.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-25.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-26.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-27.JPG",
+    "../FOTOSVIDEOS/deportes/deportes-28.JPG"
+    
 ];
 
