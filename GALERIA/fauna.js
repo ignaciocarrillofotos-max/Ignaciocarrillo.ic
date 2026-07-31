d4d6db81dc7819751ec38387ed91a683f1612e52
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-31 17:14:42 +0200

    Update fauna.js

--- GALERIA/fauna.js
+++ GALERIA/fauna.js
@@ -6,6 +6,11 @@ const galeriaActual = [
     "../FOTOSVIDEOS/fauna/fauna-02.jpg",
     "../FOTOSVIDEOS/fauna/fauna-03.jpg",
     "../FOTOSVIDEOS/fauna/fauna-04.jpg",
-    "../FOTOSVIDEOS/fauna/fauna-05.jpg"
+    "../FOTOSVIDEOS/fauna/fauna-05.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-06.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-07.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-08.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-09.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-10.jpg"
 
 ];
