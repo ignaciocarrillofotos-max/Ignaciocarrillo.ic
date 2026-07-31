1d187c239ec0fb4d81be8da19a2a51451a80a9c7
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-07-31 13:39:33 +0200

    Create fauna.js

---
+++ GALERIA/fauna.js
@@ -0,0 +1,11 @@
+const nombreGaleria = "Fauna";
+const tipoGaleria = "fotos";
+const galeriaActual = [
+
+    "../FOTOSVIDEOS/fauna/fauna-01.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-02.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-03.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-04.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-05.jpg"
+
+];
