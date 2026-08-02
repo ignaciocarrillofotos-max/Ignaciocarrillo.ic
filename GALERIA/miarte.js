adf1ed24a4e7fc24788a7b931dc9a50723fbad3c
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-08-02 11:57:03 +0200

    Update miarte.js

--- GALERIA/miarte.js
+++ GALERIA/miarte.js
@@ -9,8 +9,8 @@ const galeriaActual = [
     "../FOTOSVIDEOS/miarte/miarte-05.jpg",
     "../FOTOSVIDEOS/miarte/miarte-06.jpg",
     "../FOTOSVIDEOS/miarte/miarte-07.jpg",
-    "../FOTOSVIDEOS/miarte/miarte-08.JPG",
-    "../FOTOSVIDEOS/miarte/miarte-09.JPG",
+    "../FOTOSVIDEOS/miarte/miarte-08.PNG",
+    "../FOTOSVIDEOS/miarte/miarte-09.PNG",
     "../FOTOSVIDEOS/miarte/miarte-10.jpg",
     "../FOTOSVIDEOS/miarte/miarte-11.jpg"
 
