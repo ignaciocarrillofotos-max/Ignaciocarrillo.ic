f84d1eb5730d36a20bd6279db9f996e49d111478
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-08-02 12:51:46 +0200

    Update sesiones_personales.js

--- GALERIA/sesiones_personales.js
+++ GALERIA/sesiones_personales.js
@@ -1,1 +1,30 @@
+const nombreGaleria = "paisajes";
+const tipoGaleria = "fotos";
+const galeriaActual = [
 
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-01.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-02.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-03.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-04.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-05.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-06.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-07.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-08.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-09.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-10.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-11.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-12.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-13.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-14.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-15.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-16.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-17.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-18.JPG",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-19.JPG",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-20.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-21.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-22.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-23.jpg",
+    "../FOTOSVIDEOS/sesiones_personales/sesiones_personales-24.jpg"
+
+];
