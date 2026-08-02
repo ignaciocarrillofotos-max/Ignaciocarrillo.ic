e2a3e463b53ed5bd99f6b35bceac12b8e15a2e89
Ignaciocarrillo.ic <ignaciocarrillo.fotos@gmail.com>
2026-08-02 11:27:46 +0200

    Fix image extensions in fauna gallery
    Updated image file extensions for fauna gallery.

--- GALERIA/fauna.js
+++ GALERIA/fauna.js
@@ -3,8 +3,8 @@ const tipoGaleria = "fotos";
 const galeriaActual = [
 
     "../FOTOSVIDEOS/fauna/fauna-01.jpg",
-    "../FOTOSVIDEOS/fauna/fauna-02.png",
-    "../FOTOSVIDEOS/fauna/fauna-03.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-02.jpg",
+    "../FOTOSVIDEOS/fauna/fauna-03.png",
     "../FOTOSVIDEOS/fauna/fauna-04.jpg",
     "../FOTOSVIDEOS/fauna/fauna-05.jpg",
     "../FOTOSVIDEOS/fauna/fauna-06.jpg",
